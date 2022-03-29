55ddde7b474dc4a4a69f43077fef903103ffe8c8
SuryanshShahi <suryansh06shahi@gmail.com>
2022-03-29 23:55:56 +0530

    update

---
+++ client/src/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+export class Header extends Component {
+    render() {
+        return (
+            <div className='Header'>
+                <div className='Header-container'>
+                    <div>
+                        <p>Optimize your <span>data</span></p><i className="fa fa-sort"></i>
+                    </div>
+                    <div className='Header-dashboard-features'>
+                        <Link to="" className='Header-edit-dashboard'>Edit Dashboard</Link>
+                        <Link to="" className='Header-add-data'>Add Data</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
