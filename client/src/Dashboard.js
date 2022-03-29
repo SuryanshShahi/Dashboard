02f46c2e5b86170752c131ed4f603b39182b714b
SURYANSH SHAHI <70589762+SuryanshShahi@users.noreply.github.com>
2022-03-30 02:37:15 +0530

    Update Dashboard.js

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -21,7 +21,7 @@ export class Dashboard extends Component {
         this.updateWindowDimensions();
         window.addEventListener('resize', this.updateWindowDimensions);
         window.addEventListener('load', this.updateWindowDimensions);
-        axios.get('http://localhost:5000/')
+        axios.get('http://localhost:5000/Dashboard')
             .then(res => (
                 this.setState({ dashboardData: res.data })
             ))
