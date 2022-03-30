31e5ccf2c2f19912c9dfca5e4b20ecd1a26d8cb9
SURYANSH SHAHI <70589762+SuryanshShahi@users.noreply.github.com>
2022-03-30 11:17:03 +0530

    Update Dashboard.js

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -21,7 +21,7 @@ export class Dashboard extends Component {
         this.updateWindowDimensions();
         window.addEventListener('resize', this.updateWindowDimensions);
         window.addEventListener('load', this.updateWindowDimensions);
-        axios.get('https://dashboard06.herokuapp.com/')
+        axios.get('http://localhost:5000/')
             .then(res => (
                 this.setState({ dashboardData: res.data })
             ))
